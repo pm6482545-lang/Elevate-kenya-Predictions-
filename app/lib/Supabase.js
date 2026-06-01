dede87adb8733f7fec5aed4a86d8b285ca24f3cb
pm6482545-lang <pm6482545@gmail.com>
2026-06-01 19:50:22 +0300

    Create Supabase.js

---
+++ app/lib/Supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// These use the keys you already set up in your environment
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
